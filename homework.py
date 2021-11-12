49e668b5feff4d91886fa10d334176d3f0963936
Анастасия <fedyaevaAS@yandex.ru>
2021-11-12 13:38:44 +0300

    commit 5

--- homework.py
+++ homework.py
@@ -35,7 +35,7 @@ RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
@@ -51,21 +51,25 @@ def send_message(bot, message):
         )
     except Exception as error:
         error_message = f'Сбой при отправке сообщения в Telegram: {error}'
-        logging.error(error_message)
+        logger.error(error_message)
         raise CustomException(error_message)
-    else:
-        logging.info(f'Бот отправил сообщение "{message}"')
+    logger.info(f'Бот отправил сообщение "{message}"')
 
 
 def get_api_answer(current_timestamp):
     """Отправка запроса к эндпоинту API-сервиса."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except Exception as error:
+        error_message = f'Ошибка при запросе к основному API: {error}'
+        logger.error(error_message)
+        raise CustomException(error_message)
     if response.status_code != HTTPStatus.OK:
         error_message = (f'Эндпоинт {ENDPOINT} недоступен. '
                          f'Код ответа API: {response.status_code}')
-        logging.error(error_message)
+        logger.error(error_message)
         raise CustomException(error_message)
     return response.json()
 
@@ -75,8 +79,8 @@ def check_response(response):
     homeworks = response['homeworks']
     statuses = list(map(lambda homework: homework.get('status'), homeworks))
     for status in statuses:
-        if status not in HOMEWORK_STATUSES:
-            logging.error('Статус домашней работы недокументрован')
+        if status not in HOMEWORK_VERDICTS:
+            logger.error('Статус домашней работы недокументрован')
     return homeworks
 
 
@@ -84,8 +88,12 @@ def parse_status(homework):
     """Извлечение статуса домашней работы."""
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    verdict = HOMEWORK_STATUSES[homework_status]
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    verdict = HOMEWORK_VERDICTS[homework_status]
+    if homework_name is None:
+        logger.error('Отсутствует название домашней работы')
+        return f'Изменился статус проверки работы. {verdict}'
+    else:
+        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
@@ -99,46 +107,44 @@ def check_tokens():
     for name, value in variables.items():
         if value is None:
             result.append(False)
-            logging.critical('Отсутствует обязательная переменная '
+            logger.critical('Отсутствует обязательная переменная '
                              f'окружения {name}')
-    return False if False in result else True
+    return all(result)
 
 
 def main():
     """Основная логика работы бота."""
     if check_tokens() is False:
         exit()
-    else:
-        bot = telegram.Bot(token=TELEGRAM_TOKEN)
-        current_timestamp = int(time.time())
-        last_message = ''
-        while True:
-            try:
-                response = get_api_answer(current_timestamp)
-                homeworks = check_response(response)
-                if len(homeworks) > 0:
-                    last_homework = homeworks[0]
-                    status = parse_status(last_homework)
-                    send_message(bot, status)
-                    time_last_homework_str = last_homework.get('date_updated')
-                    current_timestamp = int(
-                        dt.datetime.strptime(
-                            time_last_homework_str,
-                            "%Y-%m-%dT%H:%M:%SZ"
-                        ).timestamp()
-                    )
-                    time.sleep(RETRY_TIME)
-                else:
-                    logging.debug('Статус домашней работы не изменился')
-            except Exception as error:
-                message = f'Сбой в работе программы: {error}'
-                split_message = message.split('from_date')[0]
-                if last_message != split_message:
-                    send_message(bot, message)
-                last_message = split_message
-                print(f'Последнее сообщение: {last_message}')
-                logging.error(message)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    current_timestamp = int(time.time())
+    last_message = ''
+    while True:
+        try:
+            response = get_api_answer(current_timestamp)
+            homeworks = check_response(response)
+            if len(homeworks) > 0:
+                last_homework = homeworks[0]
+                status = parse_status(last_homework)
+                send_message(bot, status)
+                time_last_homework_str = last_homework.get('date_updated')
+                current_timestamp = int(
+                    dt.datetime.strptime(
+                        time_last_homework_str,
+                        "%Y-%m-%dT%H:%M:%SZ"
+                    ).timestamp()
+                )
                 time.sleep(RETRY_TIME)
+            else:
+                logger.debug('Статус домашней работы не изменился')
+        except Exception as error:
+            message = f'Сбой в работе программы: {error}'
+            split_message = message.split('from_date')[0]
+            if last_message != split_message:
+                send_message(bot, message)
+            last_message = split_message
+            logger.error(message)
+            time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
